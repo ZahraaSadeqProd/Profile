441b28a113bd36b83dd45a179b41d19e8e6aba59
Zahraa <zkarem12@gmail.com>
2026-01-14 22:39:27 -0700

    forgot to update the tech field

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -4,7 +4,7 @@ const projects = [
     name: "Angular To-Do App",
     description:
       "A full-stack Todo app built with Angular, Node.js/Express, and MongoDB. Features include secure email-based JWT login, task management with priorities, status updates, search/sort functionality, and demo accounts for quick access. Designed with modern responsive UI for seamless user experience.",
-    tech: ["Angular", "Bootstrap"],
+    tech: ["Angular","Node.js", "Express", "MongoDB", "Bootstrap"],
     demoUrl: "https://zas-angulartodoapp.netlify.app/",
     repoUrl: "https://github.com/ZahraaSadeqProd/Angular-Todo-App",
   },
