437366e50dce5eafa58f65a1b52a8b8956e78323
Zahraa <zkarem12@gmail.com>
2026-02-02 13:59:59 -0700

    updating link

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -6,7 +6,7 @@ const projects = [
       "A full-stack Todo app built with Angular, Node.js/Express, and MongoDB. Features include secure email-based JWT login, task management with priorities, status updates, search/sort functionality, and demo accounts for quick access. Designed with modern responsive UI for seamless user experience.",
     tech: ["Angular","Node.js", "Express", "MongoDB", "Bootstrap"],
     demoUrl: "https://zas-angulartodoapp.netlify.app/",
-    repoUrl: "https://github.com/ZahraaSadeqProd/Angular-Todo-App",
+    repoUrl: "https://github.com/ZahraaSadeqProd/Angular-TodoApp",
   },
 ];
 
